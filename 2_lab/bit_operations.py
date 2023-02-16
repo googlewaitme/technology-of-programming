99525b09c024af19d081755e3adac5066f2c205d
googlewaitme <kusok.apelbsina@hotmail.com>
2023-02-16 17:22:15 +0300

    added new report and fixed bug

--- 2_lab/bit_operations.py
+++ 2_lab/bit_operations.py
@@ -1,10 +1,9 @@
 
 def pobit_task_3():
     print("pobit_task_3")
-    n = 2
-    for i in range(5):
-        n <<= 1;
-    print(n)
+    temp = 2
+    temp <<= 5;
+    print(temp)
 
 def pobit_task_6(z: int = 0):
     print("\npobit_task_6")
@@ -17,20 +16,23 @@ def pobit_task_7(z: int = 0b1111011):
     print(z, bin(z))
     z >>= 1
     z <<= 1
-    print(z, bin(z)) 
+    print(z, bin(z))
 
 def pobit_task_15(z: int = 174):
     print('\npobit_task_15')
     print(z, bin(z))
-    count_of_bits = len(str(bin(z))) - 2
+    count_of_bits = 0
+    while z > 0:
+        count_of_bits += 1
+        z >>= 1
     print('count of bits', count_of_bits)
 
 def main():
     pobit_task_3()
-    z = 0b1011010
-    pobit_task_6(z)
-    z = 0b1011011
-    pobit_task_6(z)
+    temp = 0b1011010
+    pobit_task_6(temp)
+    temp = 0b1011011
+    pobit_task_6(temp)
 
     pobit_task_7()
 
@@ -38,4 +40,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
